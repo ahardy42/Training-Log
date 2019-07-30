d3e55f6844389b7554d69ea9c6c70b9a7588b0df
andy_hardy <aohardy@gmail.com>
2019-07-29 19:45:03 -0600

    updated the structure to use mongodb and react

---
+++ routes/api-routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const path = require("path");
+const models = require("../models/Index");
+
+// =====================================================================================
+//                                         API routes
+// =====================================================================================
+
+// get all training for a user
+
+// add a training for a user
+
+// update a training for a user
+
+// delete a training for a user
+
+// get all athletes for the coach
+
