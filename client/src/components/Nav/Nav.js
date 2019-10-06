e7558069fcf77ea191b114e088ba95793e2979d7
Rafael Costa <rafaelsevla@gmail.com>
2019-10-06 01:08:29 -0300

    improve navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
+import './Nav.sass'
 
 
 const Nav = ({name, handleSignOut, isLoggedIn, pathName, type}) => {
