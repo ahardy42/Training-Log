8fe75757fd451924be28be40fcf76736617d1569
andy_hardy <aohardy@gmail.com>
2019-10-18 22:08:51 -0600

    fixed the training add input where you could enter a negative number

--- client/src/containers/TrainingModal/TrainingModal.js
+++ client/src/containers/TrainingModal/TrainingModal.js
@@ -44,12 +44,15 @@ class TrainingModal extends React.Component {
         }
     }
     checkTime = (time, val) => {
-        if (Number.isNaN(val)) {
+        console.log(time, val);
+        console.log(typeof val)
+        if (typeof val !== "number") {
             this.setState({
                 [time]: 0,
                 invalidDuration: true
             });
         } else {
+            console.log("invalid duration is false")
             this.setState({
                 invalidDuration: false
             })
@@ -101,12 +104,16 @@ class TrainingModal extends React.Component {
         // input handler for form version of modal!
         event.preventDefault();
         let {name, value} = event.target;
-        let parsedValue = Boolean(parseInt(value)) ? parseInt(value) : value; // return numbers where I need to
+        let parsedValue = Boolean(parseInt(value)) ? Math.abs(parseInt(value)) : value; // return numbers where I need to
         this.setState({
             [name]: parsedValue
         }, () => {
             if (validationFunction) {
-                validationFunction();
+                if (name === "hours" || name === "minutes") {
+                    validationFunction(name, parsedValue);
+                } else {
+                    validationFunction();
+                }
             } else {
                 return;
             }
